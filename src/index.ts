dc1af7fa122142fa09e47aaee1d83375cf2cc98f
Kiizuha Kanazawa <kiizuha@gnuweeb.org>
2025-04-07 22:55:20 +0700

    debug
    Signed-off-by: Kiizuha Kanazawa <kiizuha@gnuweeb.org>

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
-import activity from "./assets/samples/activities.json" with {type: 'json'};
-
 import axios from "axios";
 import querystring from "node:querystring";
 import {
   getApplicationIds,
   registerAllFonts,
   DISCORD_API_TOKEN,
-  DISCORD_USER_ID,
+  ACTIVITY_URL,
 } from "./lib/index.js";
 import { GameActivity } from "./lib/designer.js";
 import { Activity, ApplicationInfo } from "./lib/types";
+import { APP_INFO_URL } from "./lib/constants.js";
 
 const headers = {
   Authorization: DISCORD_API_TOKEN,
@@ -23,26 +22,21 @@ axios.interceptors.response.use(
 (async () => {
   await registerAllFonts();
 
-  const actRes = await axios.get(
-    `https://discord.com/api/v9/content-inventory/users/${DISCORD_USER_ID}/outbox`,
-    { headers }
-  );
+  const actRes = await axios.get(ACTIVITY_URL, { headers });
 
   const activity: Activity = actRes.data;
-  const entries = activity.entries
+  const entries = activity.entries;
 
-  console.log(activity, entries, actRes, headers, headers.Authorization?.length)
+  console.log(activity, entries, actRes, headers, headers.Authorization?.length);
 
   const queryApps = querystring.stringify({
     application_ids: getApplicationIds(entries),
   });
-  const APP_INFO_URL = `https://discord.com/api/v9/applications/public?${queryApps}`;
-
-  const appRes = await axios.get(APP_INFO_URL, { headers });
+  const appRes = await axios.get(APP_INFO_URL + queryApps, { headers });
   const apps: ApplicationInfo[] = appRes.data;
 
   const initialHeight = 75;
-  const activityBoxHeight = 170
+  const activityBoxHeight = 170;
   const padding = 20;
   const adaptiveHeight = initialHeight + padding * 2 + activityBoxHeight * entries.length;
 
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -6,3 +6,4 @@ export const DISCORD_USER_ID = process.env.DISCORD_USER_ID;
 export const DISCORD_API_TOKEN = process.env.DISCORD_API_TOKEN;
 
 export const ACTIVITY_URL = `https://discord.com/api/v9/content-inventory/users/${DISCORD_USER_ID}/outbox`;
+export const APP_INFO_URL = "https://discord.com/api/v9/applications/public?";
